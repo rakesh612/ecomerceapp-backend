ff3208f6be3f10c32acb0a101371a0572ef22f33
Nukala Hanumath Rakesh <rakeshcoc1to11@example.com>
2025-04-07 19:52:25 +0530

    cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -21,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser()); // allows you to parse cookies in the request
+app.use(cors()); // allows you to send cookies to the server
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
